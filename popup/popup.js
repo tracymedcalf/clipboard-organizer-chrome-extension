f24ac89269d8bd5a08bc846e9d6168fcb4639d3a
Tracy Medcalf <20527084+tracymedcalf@users.noreply.github.com>
2023-05-31 13:03:14 -0700

    renamed

--- popup/popup.js
+++ popup/popup.js
@@ -1,12 +1,13 @@
 const el = document.getElementById("save-text");
 
-el.onclick = async function() {
+el.onclick = function() {
     console.log("button clicked");
-    await browser.cookies.set({
-        name: "cover_letter_saved_data",
-        value: JSON.stringify({ text: 'test' }),
-        url: "www.upwork.com"
-    });
+    console.log(chrome.storage);
+    //await browser.cookies.set({
+    //    name: "cover_letter_saved_data",
+    //    value: JSON.stringify({ text: 'test' }),
+    //    url: "www.upwork.com"
+    //});
 
     //browser.cookies.get({
     //    name: "cover_letter_saved_data",
