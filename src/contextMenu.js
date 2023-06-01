445746b62939c197f2c3cc3a1627459177e40e2b
Tracy Medcalf <20527084+tracymedcalf@users.noreply.github.com>
2023-06-01 13:26:28 -0700

    adding in OpenAI calls

--- src/contextMenu.js
+++ src/contextMenu.js
@@ -1,8 +1,47 @@
-//import { key } from "./storage_key"
+import { Configuration, OpenAIApi } from "openai";
+import { key } from "./storage_key"
 
-// Store text to persist while plugin is loaded or until reset.
-async function store(text) {
-    const key = "upwork_cover_letter_generator_data";
+const configuration = new Configuration({
+    // CHANGE
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+async function (req, res) {
+    if (!configuration.apiKey) {
+        console.error("OpenAI API key not configured, please follow instructions in README.md");
+        return;
+    }
+
+    const jobDescription = "ATTENTION: PLACEHOLDER";
+    try {
+        const completion = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: generatePrompt(jobDescription),
+            temperature: 0.6,
+        });
+        res.status(200).json({ result: completion.data.choices[0].text });
+    } catch(error) {
+        // Consider adjusting the error handling logic for your use case
+        if (error.response) {
+            console.error(error.response.status, error.response.data);
+            res.status(error.response.status).json(error.response.data);
+        } else {
+            console.error(`Error with OpenAI API request: ${error.message}`);
+            res.status(500).json({
+                error: {
+                    message: 'An error occurred during your request.',
+                }
+            });
+        }
+    }
+}
+
+// Patch the job description into my resume.
+function generatePrompt(jobDescription) {
+}
+
+// Store text to persist while plugin is loaded or until reset. async function store(text) {
 
     const o = await chrome.storage.local.get([key]);
 
