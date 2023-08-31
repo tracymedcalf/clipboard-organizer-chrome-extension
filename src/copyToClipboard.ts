a15a431fae9f1af19b0a07f3b7132e172e5235e5
tracymedcalf <20527084+tracymedcalf@users.noreply.github.com>
2023-08-31 09:57:15 -0700

    Added appropriate attribution

--- src/copyToClipboard.ts
+++ src/copyToClipboard.ts
@@ -4,6 +4,12 @@ async function copyToClipboard() {
     const store: Store = await Store.get();
 
     const text = Store.toText(store);
+
+    // For the below code, credit goes to the following post and StackOverflow
+    // user:
+    // https://stackoverflow.com/a/18455088
+    // https://stackoverflow.com/users/835766/jeff-gran
+
     var copyFrom = document.createElement("textarea");
 
     copyFrom.textContent = text;
