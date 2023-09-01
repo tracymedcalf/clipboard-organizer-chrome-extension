b354ec8d9922cb353c5e4a7f1b9f2186f04f0e77
tracymedcalf <20527084+tracymedcalf@users.noreply.github.com>
2023-09-01 10:43:40 -0700

    Split webpack configs into dev and prod

--- webpack.common.js
+++ webpack.common.js
@@ -1,18 +1,12 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    mode: "development",
-    devtool: "inline-source-map",
     entry: {
         content_script: './src/content_script.ts',
         popup: './src/popup.tsx',
         options: './src/options.tsx',
         background: './src/background.ts'
-    },
-    devServer: {
-        hot: true,
     },
     output: {
         filename: '[name].js',
