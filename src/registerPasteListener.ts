9bd2ad65727dd4e5c176b905780d9f503116e5c2
tracymedcalf <20527084+tracymedcalf@users.noreply.github.com>
2023-09-01 09:28:18 -0700

    1 less level of indent., 1 less any type

--- src/registerPasteListener.ts
+++ src/registerPasteListener.ts
@@ -1,15 +1,17 @@
+function onMessageListener(message: string, sender: chrome.runtime.MessageSender) {
+    if (chrome.runtime.id !== sender.id) {
+        return;
+    }
+
+    const el = document.activeElement as any;
+    const [start, end] = [el.selectionStart, el.selectionEnd];
+    el.setRangeText(message, start, end, 'select');
+
+    el.dispatchEvent(new Event('input', { bubbles: true }));
+}
+
 function registerPasteListener() {
-    chrome.runtime.onMessage.addListener((message: string, sender: any) => {
-        if (chrome.runtime.id !== sender.id) {
-            return;
-        }
-
-        const el = document.activeElement as any;
-        const [start, end] = [el.selectionStart, el.selectionEnd];
-        el.setRangeText(message, start, end, 'select');
-
-        el.dispatchEvent(new Event('input', { bubbles: true}));
-    });
+    chrome.runtime.onMessage.addListener(onMessageListener);
 }
 
 export default registerPasteListener;
